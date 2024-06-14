369e05520d3403382cd587a3acfe07fff847b5be
naydy <e.sebastian1978@gmail.com>
2024-06-14 09:55:05 +0100

    Header and Footer created

--- clinic/src/layouts/mainLayout.jsx
+++ clinic/src/layouts/mainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 function Root() {
   return (
     <div>
-        <h1>Header</h1>
+        <Header />
         <Outlet />
-        <h1>Footer</h1>
+        <Footer />
     </div>
   )
 }
